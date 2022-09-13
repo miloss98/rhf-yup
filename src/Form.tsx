dff231493b6730409b54755266f849d02a143f60
stojanovic98m <stojanovic98m@gmail.com>
2022-09-13 12:33:12 +0200

    first validation done

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,12 +1,27 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "./style.css";
 
 const Form = () => {
+  const schema = yup.object().shape({
+    firstName: yup.string().min(3).max(32).required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).max(24).required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null])
+      .required(),
+  });
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    // formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data: any) => {
     console.log(data);
